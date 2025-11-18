3d1d4d1f3bb78babe97960df81da25c6704e112e
skmishra2801 <skmishra2801@gmail.com>
2025-11-18 10:15:54 +0530

    Initial Commit

--- config.py
+++ config.py
@@ -1,18 +1,21 @@
 import os
 from dotenv import load_dotenv
 
-
+load_dotenv()
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret")
+    MYSQL_HOST = os.getenv("MYSQL_HOST")
+    MYSQL_USER = os.getenv("MYSQL_USER")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+    MYSQL_DB = os.getenv("MYSQL_DB")
+    MYSQL_PORT = os.getenv("MYSQL_PORT")
+
 
     SQLALCHEMY_DATABASE_URI = (
-        f"mysql+pymysql://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}"
-        f"@{os.getenv('MYSQL_HOST')}:{os.getenv('MYSQL_PORT')}/{os.getenv('MYSQL_DB')}"
+        f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}"
+        f"@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
     )
 
-    # SQLALCHEMY_DATABASE_URI = (
-    #     "mysql+pymysql://avnadmin:AVNS_m3B8hIbb_zFmshELa10@mysql-1c9fb5e6-skmishra2801-3a55-cpl.i.aivencloud.com:10985/CPL"
-    # )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     ENVIRONMENT = os.environ.get("ENVIRONMENT", "development")
